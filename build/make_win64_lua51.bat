db9ed93db27ef909b476ea2d212419693b8e4e86
Sol <jiezuowhu@163.com>
2023-04-11 15:24:44 +0800

    Update make_win64_lua51.bat
    xlua5.1生成

--- build/make_win64_lua51.bat
+++ build/make_win64_lua51.bat
@@ -1,5 +1,5 @@
 mkdir build64 & pushd build64
-cmake -G "Visual Studio 16 2019" -A x64 ..
+cmake -DLUA_VERSION=5.1.5 -G "Visual Studio 16 2019" -A x64 ..
 popd
 cmake --build build64 --config Release
 md plugin_lua51\Plugins\x86_64
