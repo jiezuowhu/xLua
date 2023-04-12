8c3244cc46032c42478721ab3de01a91f7622ac2
Sol <jiezuowhu@163.com>
2023-04-12 13:55:39 +0800

    pb

--- build/pb/pb.c
+++ build/pb/pb.c
@@ -29,7 +29,7 @@
 #include <lualib.h>
 #include <lauxlib.h>
 
-#ifdef _WIN32_WCE
+#ifdef _WIN32
 #define PACKED_DECL 
 #pragma pack(1)
 #else
@@ -45,7 +45,7 @@ struct __una_16  { int16_t  x PACKED_DECL; };
 struct __una_f   { float    x PACKED_DECL; };
 struct __una_d   { double   x PACKED_DECL; };
 
-#ifdef _WIN32_WCE
+#ifdef _WIN32
 #pragma pack
 #endif
 
