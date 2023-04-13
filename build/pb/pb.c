8cf4442388fc79c84f40d2de9c5c1e57fdb28965
Sol <jiezuowhu@163.com>
2023-04-13 11:27:49 +0800

    兼容WSA

--- build/pb/pb.c
+++ build/pb/pb.c
@@ -32,13 +32,13 @@
 /*
 ** Windows stuff
 */
-#if defined(_WIN32) || defined(_MSC_VER) 	/* { */
+//#if defined(_WIN32) || defined(_MSC_VER) 	/* { */
 
 #if !defined(_CRT_SECURE_NO_WARNINGS)
 #define _CRT_SECURE_NO_WARNINGS  /* avoid warnings about ISO C functions */
 #endif
 
-#endif			/* } */
+//#endif			/* } */
 
 #ifdef _WIN32
 #define PACKED_DECL 
